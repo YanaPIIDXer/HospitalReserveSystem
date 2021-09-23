c6c7dfd482265701f80975a434ed54ede64f19a3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-23 09:03:53 +0900

    不要になったものを撤去

--- frontend/src/src/App.vue
+++ frontend/src/src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <div id="app">
-    <HelloWorld />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
