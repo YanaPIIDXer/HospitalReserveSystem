a86112480257b52aa939a46b62769b88f53e42af
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 07:54:03 +0900

    名前と予約情報を取ってくるようにした

--- frontend/src/src/components/pages/PatientReserve.vue
+++ frontend/src/src/components/pages/PatientReserve.vue
@@ -1,16 +1,26 @@
 <template>
     <div>
-        <h1>患者の予約情報</h1>
+        <h1>{{this.name}} 様</h1>
     </div>
 </template>
 
 <script>
+import { get } from '../../modules/APIConnection';
+
 export default {
     name: "PatientReserve",
     data: function() {
         return {
-            id: this.$route.params.id
+            id: this.$route.params.id,
+            name: "",
+            reserves: []
         };
+    },
+    mounted: async function() {
+        const result = await get("reserve/patient_reserve.php?id=" + this.id);
+        const json = JSON.parse(JSON.stringify(result.data));
+        this.name = json.name;
+        this.reserves = json.reserve;
     }
 }
 </script>
