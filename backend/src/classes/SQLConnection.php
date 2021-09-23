9805d008abdb1e58cd3d9cbdff6539ca44aa1a88
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-23 09:19:15 +0900

    色々とメソッド定義

--- backend/src/classes/SQLConnection.php
+++ backend/src/classes/SQLConnection.php
@@ -1,13 +1,24 @@
 <?php
     // SQLへの接続クラス
     class SQLConnection {
-        private $pdo = null;
+        private $pdo = null;        // PDOオブジェクト
 
         // 生成
-        static function create() {
+        public static function create() {
             $conn = new SQLConnection();
             if (!$conn->create_pdo()) { return null; }
             return $conn;
+        }
+
+        // クエリ実行
+        public function query($query) {
+            $stmt = $pdo->query($query);
+            return $stmt;
+        }
+
+        // 直接実行
+        public function execute($query) {
+            return $pdo->execute($query);
         }
 
         // PDOオブジェクト生成
