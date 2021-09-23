23439a78b8d9e0b5f73fb7ea04bc6c31cfa4b3fa
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 03:00:01 +0900

    患者情報のモック

---
+++ backend/src/patient/list.php
@@ -0,0 +1,11 @@
+<?php
+    // ※モック
+    $response = [
+        ["name" => "aaa", "address" => "xxx", "tel" => "0000000000"],
+        ["name" => "bbb", "address" => "yyy", "tel" => "0000111000"],
+        ["name" => "ccc", "address" => "zzz", "tel" => "0000000111"],
+        ["name" => "ddd", "address" => "xyz", "tel" => "0222000000"],
+        ["name" => "eee", "address" => "zyx", "tel" => "0000546000"]
+    ];
+    echo json_encode($response);
+?>
