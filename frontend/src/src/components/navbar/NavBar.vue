208c1f5fc2c98b28a8a338f5e698866dea4aae5b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 07:57:42 +0900

    インデックスが狂っていたので修正

--- frontend/src/src/components/navbar/NavBar.vue
+++ frontend/src/src/components/navbar/NavBar.vue
@@ -3,9 +3,9 @@
         <p class="navbar-brand">診療予約システム</p>
         <div class="navbar-nav">
             <nav-bar-item text="トップ" path="/" index=0 :selectedIndex="selectedIndex" @on-click="onClickItem" />
-            <nav-bar-item text="患者一覧" path="/patient_list" index=0 :selectedIndex="selectedIndex" @on-click="onClickItem" />
-            <nav-bar-item text="患者追加" path="/register_patient" index=1 :selectedIndex="selectedIndex" @on-click="onClickItem" />
-            <nav-bar-item text="予約一覧" path="reserve_list" index=2 :selectedIndex="selectedIndex" @on-click="onClickItem" />
+            <nav-bar-item text="患者一覧" path="/patient_list" index=1 :selectedIndex="selectedIndex" @on-click="onClickItem" />
+            <nav-bar-item text="患者追加" path="/register_patient" index=2 :selectedIndex="selectedIndex" @on-click="onClickItem" />
+            <nav-bar-item text="予約一覧" path="reserve_list" index=3 :selectedIndex="selectedIndex" @on-click="onClickItem" />
         </div>
     </nav>
 </template>
