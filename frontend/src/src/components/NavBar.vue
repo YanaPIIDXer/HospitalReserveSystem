06f780f266aa5356b2407ffe89c22145324e958b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-23 22:15:48 +0900

    ナビゲーションバーを仮で実装

--- frontend/src/src/components/NavBar.vue
+++ frontend/src/src/components/NavBar.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-        <h1>ナビゲーションバー</h1>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <p class="navbar-brand">診療予約システム</p>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="#">患者一覧</a>
+            <a class="nav-item nav-link" href="#">患者追加</a>
+            <a class="nav-item nav-link" href="#">予約情報</a>
+        </div>
+    </nav>
 </template>
 
 <script>
