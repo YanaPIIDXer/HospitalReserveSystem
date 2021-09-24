20a9b082c9659b6b727b13355b6fd5b511b139b3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 09:43:40 +0900

    日本時間の時刻を参照するようにした

--- backend/src/api/reserve/list.php
+++ backend/src/api/reserve/list.php
@@ -1,7 +1,9 @@
 <?php
     require_once "../../config/allow_cors.php";
     require_once "../../classes/SQLConnection.php";
-
+    
+    date_default_timezone_set('Asia/Tokyo');
+    
     $conn = SQLCOnnection::create();
     $stmt = $conn->prepare("SELECT patients.name, reserves.date_and_time FROM patients INNER JOIN reserves ON patients.id = reserves.patient_id WHERE reserves.date_and_time >= :now ORDER BY reserves.date_and_time ASC;");
     $stmt->bind_string(":now", date("Y/m/d H:i:s"));
