5dedb34db62009d777c6aeee9dcf8b5dfacffa85
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-24 10:29:05 +0900

    日時を表示

--- frontend/src/src/components/pages/ReserveEdit.vue
+++ frontend/src/src/components/pages/ReserveEdit.vue
@@ -1,16 +1,33 @@
 <template>
     <div>
         <h1>予約変更・取消</h1>
+        <input type="date" v-model="date" />
+        <input type="time" v-model="time" />
     </div>
 </template>
 
 <script>
+import { get } from '../../modules/APIConnection';
+
 export default {
     name: "ReserveEdit",
     data: function() {
         return {
-            id: this.$route.params.id
+            id: this.$route.params.id,
+            date: null,
+            time: null
         };
+    },
+    mounted: async function() {
+        const result = await get("reserve/get_by_id.php?id=" + this.id);
+        const json = JSON.parse(JSON.stringify(result.data));
+        if (!json.result) {
+            alert("不正なIDです");
+            return;
+        }
+        const datetime = new Date(json.datetime);
+        this.date = datetime.getFullYear() + "-" + ("00" + (datetime.getMonth() + 1)).slice(-2) + "-" + ("00" + datetime.getDate()).slice(-2);
+        this.time = ("00" + datetime.getHours()).slice(-2) + ":" + ("00" + datetime.getMinutes()).slice(-2);
     }
 }
 </script>
